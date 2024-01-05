5ec986f0b5041b889f66ee50e67b4c7037ece7a5
ImCheri <ichebotareva@yahoo.com>
2024-01-06 03:46:26 +0700

    added changes

--- Homework1/script.js
+++ Homework1/script.js
@@ -1,4 +1,4 @@
-function getSeason(monthNumber) {
+/*function getSeason(monthNumber) {
     if (monthNumber < 1 || monthNumber > 12 || isNaN(monthNumber)) {
         alert('Ошибка: введен неверный номер месяца');
     } else if (monthNumber === 12 || monthNumber <= 2) {
@@ -11,3 +11,49 @@ function getSeason(monthNumber) {
         alert('Осень');
     }
 }
+function getSeason(month) {
+  if (month === 12 || month === 1 || month === 2) {
+    alert("Это Зима!");
+  } else if (month >= 3 && month <= 5) {
+    alert("Это Весна!");
+  } else if (month >= 6 && month <= 8) {
+    alert("Это Лето!");
+  } else if (month >= 9 && month <= 11) {
+    alert("Это Осень!");
+  } else {
+    alert("Некорректный номер месяца");
+  }
+}
+
+// Пример использования функции
+let month = 7;
+getSeason(month);*/
+
+
+
+
+
+function getSeason() {
+    let month = prompt ("Введите номер месяца (1-12):");
+    if (month === null || month === "") {
+      alert("Отмена ввода");
+      return;
+    }
+    month = parseInt(month);
+    if (month >= 1 && month <= 12) {
+      if (month === 12 || month === 1 || month === 2) {
+        alert("Это Зима!");
+      } else if (month >= 3 && month <= 5) {
+        alert("Это Весна!");
+      } else if (month >= 6 && month <= 8) {
+        alert("Это Лето!");
+      } else if (month >= 9 && month <= 11) {
+        alert("Это Осень!");
+      }
+    } else {
+      alert("Некорректный номер месяца");
+    }
+  }
+  
+  // Привязываем функцию к кнопке
+  document.getElementById("seasonButton").addEventListener("click", getSeason);
